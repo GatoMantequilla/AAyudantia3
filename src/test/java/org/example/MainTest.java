835b11f556ed3817819b5c5ea37d1bb17cc0d508
GatoMantequilla <d.diaz17@ufromail.com>
2024-09-16 12:05:20 -0300

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void ejecutarJuego() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void llegoADestino() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void inicializarMapa() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generarPosicionAleatoria() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void moverJugador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void obtenerMovimiento() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calcularNuevaPosicion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void esMovimientoValido() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verificarPosicion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void iniciarCombate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void obtenerAccionCombate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void realizarAtaqueJugador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void realizarAtaqueEnemigo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void escaparDelCombate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void abrirCofre() {
+    }
+}
